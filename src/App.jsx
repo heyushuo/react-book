f17835c75f3e721bcc800b5b087bdffd1c51a486
heyushuo <1007561607@qq.com>
2018-09-14 19:07:36 +0800

    react-book

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import Header from "./common/header";
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <div className="heyuhsuo">测试样式</div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/common/header/index.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import "./style.scss";
+export default class Header extends Component {
+  render() {
+    return (
+      <div className="header">
+        <h1 className="heyushuo">我是头部信息</h1>
+      </div>
+    );
+  }
+}
